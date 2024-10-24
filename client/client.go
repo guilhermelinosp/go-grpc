0a3886bf12d86caaff0e4f5352d7728265286894
Guilherme Lino <guilhermelinosp@outlook.com>
2024-10-24 07:39:30 -0300

    up

---
+++ client/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/guilhermelinosp/go-grpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect to server: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.SayHello(ctx, &pb.HelloRequest{Name: "World"})
+	if err != nil {
+		log.Fatalf("Error calling SayHello: %v", err)
+	}
+
+	log.Println("Response status:", res.Message)
+}
---
+++ server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/guilhermelinosp/go-grpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+)
+
+type server struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	peer, ok := peer.FromContext(ctx)
+	if ok {
+		log.Printf("Request received from %s\n", peer.Addr.String())
+	}
+	return &pb.HelloResponse{Message: "Hello, " + req.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to start listener: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &server{})
+
+	log.Println("gRPC server running on port :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
